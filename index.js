a37e1c43c6c7261618d5e9ffa9b5a0aadd0e485e
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-11-27 14:37:32 +0100

    Requête test

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var unirest = require("unirest");
+
+var port = process.env.PORT || 8080;
+
+const server = express();
+
+// Test
+server.get('/getName',function (req,res){
+    res.send('Bim bada boum !');
+});
+
+// Le serveur est à l'écoute
+server.listen(port, function () {
+    console.log("Le serveur est lancé ...");
+});
