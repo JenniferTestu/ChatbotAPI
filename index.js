e08c50b56a723141f70351b94e0bb2e2ac99aad9
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-07 13:46:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,50 +3,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const restService = express();
+const serveur = express();
 
-restService.use(
+const apiCle = '44aacdb5c3e417260c94faca83d8cac0'; 
+
+serveur.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
 
-restService.use(bodyParser.json());
+serveur.use(bodyParser.json());
 
-restService.post("/meteo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+serveur.post("/meteo", function(req, res) {
+
+	let ville = req.body.queryResult.parameters.city;
+	let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ville+'&units=metric&appid='+apiCle+'&lang=fr';
+	request.get(url,(err,res,body)=>{
+		if(!err && res.statusCode == 200){
+			let json = JSON.parse(body);
+			console.log(json);
+			let temp = ~~(json.main.temp);
+			let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+temp+'°C'
+			return res.json({
+			    //payload: speechResponse,
+			    //data: speechResponse,
+			    fulfillmentText: msg,
+			    displayText: msg,
+			    source: "meteo"
+			});
+		}else{
+			let erreurMsg = 'Je ne trouve pas cette ville';
+			return res.json({
+			    //payload: speechResponse,
+			    //data: speechResponse,
+			    fulfillmentText: erreurMsg,
+			    displayText: erreurMsg,
+			    source: "meteo"
+			});
+		}
   
-  var speechResponse = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: speech
-            }
-          }
-        ]
-      }
-    }
-  };
   
-  return res.json({
-    payload: speechResponse,
-    //data: speechResponse,
-    fulfillmentText: speech,
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
 });
 
 
-restService.listen(process.env.PORT || 8080, function() {
-  console.log("Server up and listening");
+serveur.listen(process.env.PORT || 8080, function() {
+  console.log("Le serveur est lancé ...");
 });
