b04e6d788912ce04543d32453a4095c926992cdd
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-07 12:47:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,52 @@
+"use strict";
+
 const express = require("express");
-const unirest = require("unirest");
-const bodyParser = require('body-parser');
-const request = require('request');
-const axios = require('axios');
+const bodyParser = require("body-parser");
 
-const apiCle = '44aacdb5c3e417260c94faca83d8cac0'; 
+const restService = express();
 
-var port = process.env.PORT || 8080;
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-const server = express();
+restService.use(bodyParser.json());
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
-
-// Test
-server.get('/',function (req,res){
-    res.send('Démarrage réussi !');
-});
-
-server.get('/getName',function (req,res){
-	let responseObj={
-			     "fulfillmentText":'Bim bam boum, ca pshit et ca fait vroum'
-			    ,"fulfillmentMessages":[
-			        {
-			            "text": {
-			                "text": [
-			                    "Hello I m Responding to intent"
-			                ]
-			            }
-			        }
-			    ]
-			    ,"source":""
-			}
-    res.send(responseObj);
-    return res.json(responseObj);
-});
-
-// Le serveur est à l'écoute
-server.listen(port, function () {
-    console.log("Le serveur est lancé ...");
-});
-
-// Météo
-server.post('/meteo',function (req,res){
-
-		console.log(req.body.result);
-
-
-    	let ville = req.body.city;
-		let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ville+'&units=metric&appid='+apiCle+'&lang=fr';
-			request.get(url,(err,res,body)=>{
-		if(!err && res.statusCode == 200){
-			let json = JSON.parse(body);
-			console.log(json);
-			let temp = ~~(json.main.temp);
-			let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+temp+'°C'
-			//res.send(createTextResponse(msg));
-			/*return res.json({
-				speech: msg,
-				displayText: msg,
-				source: 'meteo'
-			});*/
-
-
-		}else{
-			let erreurMsg = 'Je ne trouve pas cette ville';
-			return res.status(400).json({
-				status: {
-					code: 400,
-					errorType: erreurMsg
-				}
-			});
-		}
-	})
-});
-
-function createTextResponse(msg){
-  let response = {
-    "fulfillmentText": msg,
-    "fulfillmentMessages": [
-      {
-        "text": {
-          "text": [
-            msg
-          ]
-        }
-      }
-    ],
-    "source": "example.com",
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": msg
-              }
+restService.post("/meteo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
             }
-          ]
-        }
-      },
-      "facebook": {
-        "text": msg
-      },
-      "slack": {
-        "text": msg
+          }
+        ]
       }
     }
-  }
-  return response;
-}
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+
+restService.listen(process.env.PORT || 8080, function() {
+  console.log("Server up and listening");
+});
