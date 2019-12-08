5489f651eac943aaf8995caf00a9de92245fe7f7
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 20:50:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const apiCle = '44aacdb5c3e417260c94faca83d8cac0';
 
 var port = process.env.PORT || 8080;
 
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json());
 
 // Test
@@ -19,41 +19,47 @@ server.get('/',function (req,res){
 });
 
 // Meteo
-server.post('/', function (req, res) {
+server.post('/webhook', function (req, res) {
+
+	var action = req.body.queryResult.parameters.Action;
+    
+    switch(action) {
+
+		case 'interrogation_openweathermap': {
+			var city = req.body.queryResult.parameters.City;
+
+			var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+apiCle+'&lang=fr';
+			request.get(url,(err,response,body)=>{
+			    if(!err && response.statusCode == 200){
+			      let json = JSON.parse(body);
+			      console.log(json);
+			      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+json.main.temp+'°C';
+			      //res.setHeader('Content-Type', 'application/json');
+			      /*res.send({
+			        fulfillmentMessages: msg,
+			        fulfillmentText: msg,
+			        source: 'meteo'
+			      });*/
+			      return res.json({
+			        "fulfillmentMessages": msg,
+			        "fulfillmentText": msg,
+			        "source": ""
+			      });
 
 
-	var city = req.query.city;
-
-	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+apiCle+'&lang=fr';
-	request.get(url,(err,response,body)=>{
-	    if(!err && response.statusCode == 200){
-	      let json = JSON.parse(body);
-	      console.log(json);
-	      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+json.main.temp+'°C';
-	      //res.setHeader('Content-Type', 'application/json');
-	      /*res.send({
-	        fulfillmentMessages: msg,
-	        fulfillmentText: msg,
-	        source: 'meteo'
-	      });*/
-	      return res.json({
-	        "fulfillmentMessages": msg,
-	        "fulfillmentText": msg,
-	        "source": ""
-	      });
-
-
-	    }else{
-	      let erreurMsg = 'Je ne trouve pas cette ville';
-	      return res.status(400).json({
-	        "status": {
-	          "code": 400,
-	          "errorType": erreurMsg
-	        }
-	      });
-	    }
-  	})
-
+			    }else{
+			      let erreurMsg = 'Je ne trouve pas cette ville';
+			      return res.status(400).json({
+			        "status": {
+			          "code": 400,
+			          "errorType": erreurMsg
+			        }
+			      });
+			    }
+	  		})
+		}
+		break;
+	}
 	
 });
 
