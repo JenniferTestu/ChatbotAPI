7e80b2e50f610145a89e108842947c826be73e9b
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 03:18:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require('request');
 
 const serveur = express();
 
@@ -13,47 +14,49 @@ serveur.use(bodyParser.urlencoded({extended: true}));
 serveur.use(bodyParser.json());
 
 // Test
-server.get('/',function (req,res){
+serveur.get('/',function (req,res){
     res.send('Démarrage réussi !');
 });
 
 // Meteo
 serveur.post("/meteo", function (req, res) {
 
-   if (req.body.queryResult.parameters.city) {
+   if (req.query.city) {
 	    let ville = req.query.city;
 	    let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ville+'&units=metric&appid='+apiCle+'&lang=fr';
 
-	    if(!err && response.statusCode == 200){
-		      let json = JSON.parse(body);
-		      console.log(json);
-		      let temp = ~~(json.main.temp);
-		      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+temp+'°C';
+	    request.get(url,(err,response,body)=>{
+		    if(!err && response.statusCode == 200){
+			      let json = JSON.parse(body);
+			      console.log(json);
+			      let temp = ~~(json.main.temp);
+			      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+temp+'°C';
 
-				  return res.json({
-				    fulfillmentText: msg,
-				    fulfillmentMessages: [{
-				      simpleResponses: {
-				        simpleResponses: [{
-				          "textToSpeech": "textToSpeech",
-				          "displayText": msg
-				        }]
-				      }
-				    }],
-				    source: "webhook-sample"
-				  });
-		}else{
-		      let erreurMsg = 'Je ne trouve pas cette ville';
-		      return res.status(400).json({
-		        status: {
-		          code: 400,
-		          errorType: erreurMsg
-		        }
-		      });
-    	}
+					  return res.json({
+					    fulfillmentText: msg,
+					    fulfillmentMessages: [{
+					      simpleResponses: {
+					        simpleResponses: [{
+					          "textToSpeech": "textToSpeech",
+					          "displayText": msg
+					        }]
+					      }
+					    }],
+					    source: "webhook-sample"
+					  });
+			}else{
+			      let erreurMsg = 'Je ne trouve pas cette ville';
+			      return res.status(400).json({
+			        status: {
+			          code: 400,
+			          errorType: erreurMsg
+			        }
+			      });
+	    	}
+   		});
 	}
 });
 
-server.listen(port, function () {
+serveur.listen(port, function () {
   console.log("Le serveur est lancé ...");
 });
