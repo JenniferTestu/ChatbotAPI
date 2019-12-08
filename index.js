2f7fd9ed3232a2c8bbd9dae42af0d0d286f7b0b1
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 16:16:26 +0100

    Update

--- index.js
+++ index.js
@@ -4,51 +4,60 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require('request');
 
-const serveur = express();
+const server = express();
 
 const apiCle = '44aacdb5c3e417260c94faca83d8cac0'; 
 
 var port = process.env.PORT || 8080;
+var result;
 
-serveur.use(bodyParser.urlencoded({extended: false}));
-serveur.use(bodyParser.json());
-
-// Test
-serveur.get('/',function (req,res){
-    res.send('Démarrage réussi !');
-});
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
 
 // Meteo
-serveur.post('/meteo', function (req, res) {
+server.post('/webhook', function (req, res) {
 
-   if (req.query.city) {
-	    let ville = req.query.city;
-	    let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ville+'&units=metric&appid='+apiCle+'&lang=fr';
-
-	    request.get(url,(err,response,body)=>{
-
-			      let json = JSON.parse(body);
-			      //console.log(json);
-			      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+json.main.temp+'°C';
-
-					  return res.json({
-					    fulfillmentText: msg,
-					    fulfillmentMessages: [{
-					      simpleResponses: {
-					        simpleResponses: [{
-					          "textToSpeech": "textToSpeech",
-					          "displayText": msg
-					        }]
-					      }
-					    }],
-					    source: "webhook-sample"
-					  });
-			
-   		});
-	}
+	if(!req.query)return res.sendStatus(400)
+	res.setHeader('Content-Type','application/json');
+	var city = req.query['geo-city'];
+	var w = getWeather(city);
+	let response = " ";
+	let responseObj={
+           "fulfillmentText":response
+          ,"fulfillmentMessages":[
+              {
+                  "text": {
+                      "text": [w]
+                  }
+              }
+          ]
+          ,"source":""
+    }
+    return res.json(responseObj);
 });
 
-serveur.listen(port, function () {
+function cb (err,response,body){
+	if(err){
+		console.log('error: ',error);
+	}
+	var weather = JSON.parse(body)
+	if(weather.message==='Je ne trouve pas cette ville'){
+		result='Impossible de trouver la météo '+weather.message;
+	}else{
+		result='Le temps à '+weather.name+' est '+weather.weather[0].description+' et la température est de '+weather.main.temp+'°C';
+	}
+}
+
+function getWeather(city){
+	result=undefined;
+	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+apiCle+'&lang=fr';
+	var req = request(url,cb);
+	while(result===undefined){
+		require('deasync').runLoopOnce();
+	}
+	return result;
+}
+
+server.listen(port, function () {
   console.log("Le serveur est lancé ...");
 });
-
