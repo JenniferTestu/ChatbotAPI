9d813aa4cc6755c12cc69479d8d3ad402edf2fcd
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 14:24:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const apiCle = '44aacdb5c3e417260c94faca83d8cac0';
 
 var port = process.env.PORT || 8080;
 
-serveur.use(bodyParser.urlencoded({extended: true}));
+serveur.use(bodyParser.urlencoded({extended: false}));
 serveur.use(bodyParser.json());
 
 // Test
@@ -19,7 +19,7 @@ serveur.get('/',function (req,res){
 });
 
 // Meteo
-serveur.post("/meteo", function (req, res) {
+serveur.post('/meteo', function (req, res) {
 
    if (req.query.city) {
 	    let ville = req.query.city;
@@ -52,56 +52,3 @@ serveur.listen(port, function () {
   console.log("Le serveur est lancé ...");
 });
 
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
-
-  // Only extend the timeout for API requests
-  if (!req.url.includes('/meteo')) {
-    next();
-    return;
-  }
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
-};
-
-serveur.use(extendTimeoutMiddleware);
