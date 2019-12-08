583bd03062bbf2a62776721178ddf36289abfb67
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 17:04:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const server = express();
 const apiCle = '44aacdb5c3e417260c94faca83d8cac0'; 
 
 var port = process.env.PORT || 8080;
-var result;
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
@@ -17,46 +16,45 @@ server.use(bodyParser.json());
 // Meteo
 server.post('/webhook', function (req, res) {
 
-	if(!req.query)return res.sendStatus(400)
+	if(!req.query){
+		return res.sendStatus(400);
+	}
 	res.setHeader('Content-Type','application/json');
 	var city = req.query.city;
-	var w = getWeather(city);
-	let response = " ";
-	let responseObj={
-           "fulfillmentText":response
-          ,"fulfillmentMessages":[
-              {
-                  "text": {
-                      "text": [w]
-                  }
-              }
-          ]
-          ,"source":""
-    }
-    return res.json(responseObj);
-});
 
-function cb (err,response,body){
-	if(err){
-		console.log('error: ',error);
-	}
-	var weather = JSON.parse(body)
-	if(weather.message==='Je ne trouve pas cette ville'){
-		result='Impossible de trouver la météo '+weather.message;
-	}else{
-		result='Le temps à '+weather.name+' est '+weather.weather[0].description+' et la température est de '+weather.main.temp+'°C';
-	}
-}
-
-function getWeather(city){
-	result=undefined;
 	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+apiCle+'&lang=fr';
-	var req = request(url,cb);
-	while(result===undefined){
-		require('deasync').runLoopOnce();
-	}
-	return result;
-}
+	request.get(url,(err,response,body)=>{
+	    if(!err && response.statusCode == 200){
+	      let json = JSON.parse(body);
+	      console.log(json);
+	      let temp = ~~(json.main.temp);
+	      let msg = 'Le temps à '+json.name+' est '+json.weather[0].description+' et la température est de '+temp+'°C';
+	      res.setHeader('Content-Type', 'application/json');
+	      res.send({
+	        fulfillmentMessages: msg,
+	        fulfillmentText: msg,
+	        source: 'meteo'
+	      });
+	      return res.json({
+	        "fulfillmentMessages": msg,
+	        "fulfillmentText": msg,
+	        "source": ""
+	      });
+
+
+	    }else{
+	      let erreurMsg = 'Je ne trouve pas cette ville';
+	      return res.status(400).json({
+	        "status": {
+	          "code": 400,
+	          "errorType": erreurMsg
+	        }
+	      });
+	    }
+  	})
+
+	
+});
 
 server.listen(port, function () {
   console.log("Le serveur est lancé ...");
