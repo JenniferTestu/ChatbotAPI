f9f5bbd1ecae89535c63afd7365394ac1876d76a
JenniferTestu <26251315+JenniferTestu@users.noreply.github.com>
2019-12-08 02:30:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ restService.use(bodyParser.json());
 
 restService.post("/meteo", function (req, res) {
 
-   if (req.body.queryResult.parameters.account_information == "contact number"
+   if (req.body.queryResult.parameters.city == "contact number"
 
-    && req.body.queryResult.parameters.account_information) {
+    && req.body.queryResult.parameters.city) {
 
     var speech = "999999999";
 
   }
 
-  else if (req.body.queryResult.parameters.account_information == "account number"
+  else if (req.body.queryResult.parameters.city == "account number"
 
-    && req.body.queryResult.parameters.account_information) {
+    && req.body.queryResult.parameters.city) {
 
     var speech = "9999999999999";
 
   }
 
-  else if (req.body.queryResult.parameters.account_information == "DOB"
+  else if (req.body.queryResult.parameters.city == "DOB"
 
-    && req.body.queryResult.parameters.account_information) {
+    && req.body.queryResult.parameters.city) {
 
     var speech = "1 Jan 2019";
 
   }
 
-  else if (req.body.queryResult.parameters.account_information == "address"
+  else if (req.body.queryResult.parameters.city == "address"
 
-    && req.body.queryResult.parameters.account_information) {
+    && req.body.queryResult.parameters.city) {
 
     var speech = " floor no 1 , Building no 1 , address";
 
